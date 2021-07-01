fcff3399506e94eb0d7775c420acd704d4c6af28
Lovemore <lovemorejokonya@gmail.com>
2021-07-01 10:24:24 -0400

    adding comments to demo

--- june-21-2021-east/W2D3/tcp-demo/client.js
+++ june-21-2021-east/W2D3/tcp-demo/client.js
@@ -1,6 +1,11 @@
+// require the net module
 const net = require('net');
+
+// define your port
 const port = 8080;
 
+// createConnection with net module 
+// and parse object with port and host
 const client = net.createConnection({
     port: port, 
     host: 'localhost',
@@ -9,18 +14,22 @@ const client = net.createConnection({
 // set the encoding to utf-8
 client.setEncoding('utf8');
 
+// send data to server using process.stdin.on(data) and client.write
 process.stdin.on('data', function(message){
     client.write(message);
 });
 
+// console log a message when connection is established using the on connect handler
 client.on('connect',function(){
     console.log("client is connected to server.");
 });
 
+// console log a message when a message is received using the on data handler
 client.on('data',function(message){
     console.log('server sent:',message);
 });
 
+// console log a message when a the connection is ended using the on end handler
 client.on('end', function(){
     console.log('client disconnected from server');
 });
--- june-21-2021-east/W2D3/tcp-demo/server.js
+++ june-21-2021-east/W2D3/tcp-demo/server.js
@@ -6,6 +6,9 @@ const server = net.createServer();
 // we need an array to store the connected clients.
 const connectedClients = [];
 
+// function to broadcast messages
+// loops through all connectedClients and sends a message
+// to all clients except the one that sent the original message
 const broadcast = function(currentClient,message){
     for (let connectedClient of connectedClients){
         console.log('testing client named:',connectedClient.name);
@@ -24,15 +27,21 @@ server.on('connection', function(client){
     // set the encoding to utf8
     client.setEncoding('utf8');
 
+    // send message to the client who's just connected
     client.write("Welcome to my awesome server! ⛵");
 
     client.on('data', function(message){
+        // a way to setName of client when they start their message with setName
         if (message.startsWith('setName ')){
             const clientName = message.replace(/setName /, '');
             console.log('setting client name to:',clientName);
+            // adding a "name" key and value to the client
+            // object.key = value
             client.name = clientName;
         }
         console.log(`Message received from client: ${client.name} - ${message}`);
+        
+        // broadcast the message
         broadcast(client,message);
     });
 
